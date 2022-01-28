2f53eaca4ff9ef767ef4629a52cc9454bced0521
xishuiinsz <xishuiofchina@163.com>
2022-01-28 21:50:56 +0800

    更新eslin规则，https://blog.csdn.net/weixin_42826294/article/details/107374982

--- src/router/modules/konva.js
+++ src/router/modules/konva.js
@@ -1,36 +1,36 @@
 /** When your routing table is too long, you can split it into small modules**/
 
-import Layout from '@/layout'
+import Layout from "@/layout";
 
 const konvaRouer = {
-  path: '/konva',
+  path: "/konva",
   component: Layout,
-  redirect: 'noRedirect',
-  name: 'Konva',
+  redirect: "noRedirect",
+  name: "Konva",
   meta: {
-    title: 'Konva',
-    icon: 'qq'
+    title: "Konva",
+    icon: "qq",
   },
   children: [
     {
-      path: 'keyboard',
-      component: () => import('@/views/konva/group'),
-      name: 'konvaGroupUngroup',
-      meta: { title: 'Konva Group', noCache: true }
+      path: "workbench",
+      component: () => import("@/views/konva/workbench"),
+      name: "Workbench",
+      meta: { title: "Konva Workbench", noCache: true },
     },
     {
-      path: 'line',
-      component: () => import('@/views/konva/line'),
-      name: 'LineChart',
-      meta: { title: 'Line Chart', noCache: true }
+      path: "keyboard",
+      component: () => import("@/views/konva/group"),
+      name: "konvaGroupUngroup",
+      meta: { title: "Konva Group", noCache: true },
     },
     {
-      path: 'mix-chart',
-      component: () => import('@/views/konva/mix-chart'),
-      name: 'MixChart',
-      meta: { title: 'Mix Chart', noCache: true }
-    }
-  ]
-}
+      path: "mix-chart",
+      component: () => import("@/views/konva/mix-chart"),
+      name: "MixChart",
+      meta: { title: "Mix Chart", noCache: true },
+    },
+  ],
+};
 
-export default konvaRouer
+export default konvaRouer;
--- src/views/konva/group.vue
+++ src/views/konva/group.vue
@@ -12,36 +12,36 @@
 </template>
 
 <script>
-import Konva from "konva";
+import Konva from 'konva'
 export default {
-  name: "konvaGroupUngroup",
+  name: 'KonvaGroupUngroup',
   components: {},
   mounted() {
-    this.initStage();
-    this.initlayer();
-    this.drawShape();
+    this.initStage()
+    this.initlayer()
+    this.drawShape()
   },
   methods: {
     // 初始化konva stage
     initStage() {
       const konvaGroupContainer = this.$el.querySelector(
-        "#konva-canvas-container"
-      );
-      const width = konvaGroupContainer.offsetWidth;
-      const height = konvaGroupContainer.offsetHeight;
+        '#konva-canvas-container'
+      )
+      const width = konvaGroupContainer.offsetWidth
+      const height = konvaGroupContainer.offsetHeight
       this.stage = new Konva.Stage({
-        container: "konva-canvas-container",
+        container: 'konva-canvas-container',
         width,
-        height,
-      });
-      this.stage.on("click", function (evt) {
-        console.log(evt);
-      });
+        height
+      })
+      this.stage.on('click', function (evt) {
+        console.log(evt)
+      })
     },
     // 初始化Konva.js this.layer
     initlayer() {
-      this.layer = new Konva.Layer();
-      this.stage.add(this.layer);
+      this.layer = new Konva.Layer()
+      this.stage.add(this.layer)
     },
     // 绘制图形
     drawShape() {
@@ -50,147 +50,147 @@ export default {
         y: 60,
         width: 100,
         height: 90,
-        fill: "red",
-        name: "rect",
-        draggable: true,
-      });
-      this.layer.add(rect1);
+        fill: 'red',
+        name: 'rect',
+        draggable: true
+      })
+      this.layer.add(rect1)
 
       var rect2 = new Konva.Rect({
         x: 250,
         y: 100,
         width: 150,
         height: 90,
-        fill: "green",
-        name: "rect",
-        draggable: true,
-      });
-      this.layer.add(rect2);
+        fill: 'green',
+        name: 'rect',
+        draggable: true
+      })
+      this.layer.add(rect2)
 
-      var tr = new Konva.Transformer();
-      this.tr = tr;
-      this.layer.add(tr);
+      var tr = new Konva.Transformer()
+      this.tr = tr
+      this.layer.add(tr)
 
       // by default select all shapes
-      tr.nodes([rect1, rect2]);
+      tr.nodes([rect1, rect2])
 
       // add a new feature, lets add ability to draw selection rectangle
       var selectionRectangle = new Konva.Rect({
-        fill: "rgba(0,0,255,0.5)",
-        visible: false,
-      });
-      this.layer.add(selectionRectangle);
-      const _this = this;
-      var x1, y1, x2, y2;
-      this.stage.on("mousedown touchstart", (e) => {
+        fill: 'rgba(0,0,255,0.5)',
+        visible: false
+      })
+      this.layer.add(selectionRectangle)
+      const _this = this
+      var x1, y1, x2, y2
+      this.stage.on('mousedown touchstart', e => {
         // do nothing if we mousedown on any shape
         if (e.target !== _this.stage) {
-          return;
+          return
         }
-        e.evt.preventDefault();
-        x1 = _this.stage.getPointerPosition().x;
-        y1 = _this.stage.getPointerPosition().y;
-        x2 = _this.stage.getPointerPosition().x;
-        y2 = _this.stage.getPointerPosition().y;
+        e.evt.preventDefault()
+        x1 = _this.stage.getPointerPosition().x
+        y1 = _this.stage.getPointerPosition().y
+        x2 = _this.stage.getPointerPosition().x
+        y2 = _this.stage.getPointerPosition().y
 
-        selectionRectangle.visible(true);
-        selectionRectangle.width(0);
-        selectionRectangle.height(0);
-      });
+        selectionRectangle.visible(true)
+        selectionRectangle.width(0)
+        selectionRectangle.height(0)
+      })
 
-      this.stage.on("mousemove touchmove", (e) => {
+      this.stage.on('mousemove touchmove', e => {
         // do nothing if we didn't start selection
         if (!selectionRectangle.visible()) {
-          return;
+          return
         }
-        e.evt.preventDefault();
-        x2 = _this.stage.getPointerPosition().x;
-        y2 = _this.stage.getPointerPosition().y;
+        e.evt.preventDefault()
+        x2 = _this.stage.getPointerPosition().x
+        y2 = _this.stage.getPointerPosition().y
 
         selectionRectangle.setAttrs({
           x: Math.min(x1, x2),
           y: Math.min(y1, y2),
           width: Math.abs(x2 - x1),
-          height: Math.abs(y2 - y1),
-        });
-      });
+          height: Math.abs(y2 - y1)
+        })
+      })
 
-      this.stage.on("mouseup touchend", (e) => {
+      this.stage.on('mouseup touchend', e => {
         // do nothing if we didn't start selection
         if (!selectionRectangle.visible()) {
-          return;
+          return
         }
-        e.evt.preventDefault();
+        e.evt.preventDefault()
         // update visibility in timeout, so we can check it in click event
         setTimeout(() => {
-          selectionRectangle.visible(false);
-        });
+          selectionRectangle.visible(false)
+        })
 
-        var shapes = _this.stage.find(".rect");
-        var box = selectionRectangle.getClientRect();
-        var selected = shapes.filter((shape) =>
+        var shapes = _this.stage.find('.rect')
+        var box = selectionRectangle.getClientRect()
+        var selected = shapes.filter(shape =>
           Konva.Util.haveIntersection(box, shape.getClientRect())
-        );
-        tr.nodes(selected);
-      });
+        )
+        tr.nodes(selected)
+      })
 
       // clicks should select/deselect shapes
-      this.stage.on("click tap", function (e) {
+      this.stage.on('click tap', function (e) {
         // if we are selecting with rect, do nothing
         if (selectionRectangle.visible()) {
-          return;
+          return
         }
 
         // if click on empty area - remove all selections
         if (e.target === _this.stage) {
-          tr.nodes([]);
-          return;
+          tr.nodes([])
+          return
         }
 
         // do nothing if clicked NOT on our rectangles
-        if (!e.target.hasName("rect")) {
-          return;
+        if (!e.target.hasName('rect')) {
+          return
         }
 
         // do we pressed shift or ctrl?
-        const metaPressed = e.evt.shiftKey || e.evt.ctrlKey || e.evt.metaKey;
-        const isSelected = tr.nodes().indexOf(e.target) >= 0;
+        const metaPressed = e.evt.shiftKey || e.evt.ctrlKey || e.evt.metaKey
+        const isSelected = tr.nodes().indexOf(e.target) >= 0
 
         if (!metaPressed && !isSelected) {
           // if no key pressed and the node is not selected
           // select just one
-          tr.nodes([e.target]);
+          tr.nodes([e.target])
         } else if (metaPressed && isSelected) {
           // if we pressed keys and node was selected
           // we need to remove it from selection:
-          const nodes = tr.nodes().slice(); // use slice to have new copy of array
+          const nodes = tr.nodes().slice() // use slice to have new copy of array
           // remove node from array
-          nodes.splice(nodes.indexOf(e.target), 1);
-          tr.nodes(nodes);
+          nodes.splice(nodes.indexOf(e.target), 1)
+          tr.nodes(nodes)
         } else if (metaPressed && !isSelected) {
           // add the node into selection
-          const nodes = tr.nodes().concat([e.target]);
-          tr.nodes(nodes);
+          const nodes = tr.nodes().concat([e.target])
+          tr.nodes(nodes)
         }
-      });
+      })
     },
     // 组合
     groupShape() {
       if (this.tr.nodes().length < 2) {
-        this.$message.error("至少需要2个图形才能组合！");
-        return;
+        this.$message.error('至少需要2个图形才能组合！')
+        return
       }
-      const group = new Konva.Group({});
-      const nodes = this.tr.nodes();
-      group.add(...nodes);
-      this.layer.add(group);
+      const group = new Konva.Group({})
+      const nodes = this.tr.nodes()
+      group.add(...nodes)
+      this.layer.add(group)
     },
     // 解除组合
     ungroupShape() {
-      console.log(this.stage);
-    },
-  },
-};
+      console.log(this.stage)
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
