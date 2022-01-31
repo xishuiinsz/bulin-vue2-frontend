b23cbb6eded462c88dbf64353dc7051f8dde5431
xishuiinsz <xishuiofchina@163.com>
2022-01-31 13:01:10 +0800

    npm i -s vue-fragment

--- src/main.js
+++ src/main.js
@@ -19,9 +19,11 @@ import './permission' // permission control
 import './utils/error-log' // error log
 
 import * as filters from './filters' // global filters
-import VueKonva from 'vue-konva';
+import VueKonva from 'vue-konva'
 
-Vue.use(VueKonva);
+Vue.use(VueKonva)
+import Fragment from 'vue-fragment'
+Vue.use(Fragment.Plugin)
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
